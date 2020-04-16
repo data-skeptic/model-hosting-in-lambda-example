e8028c6668ebbc9d8328fd2d6b16bc7c09ea8433
areeves87 <lxndr.rvs@gmail.com>
2020-04-16 15:09:32 -0700

    implement predict route

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from chalice import Chalice
 from chalicelib.db import ModelsDatabase
 from chalicelib.features import FeatureExtractor
 from chalicelib.dao.blobstore.s3.s3blobstore import S3Blobstore
+from chalicelib.utils.ludwig_qa import untokenize
 
 import os
 
 app = Chalice(app_name='mhost')
 
-if os.getenv('PRIMARY_BUCKET') is None:
-    from dotenv import load_dotenv
-    load_dotenv()
-
-access_key = os.getenv('AWS_ACCESS_KEY_ID')
-secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+access_key = os.getenv('ACCESS_KEY')
+secret_key = os.getenv('SECRET_KEY')
 bucket_name     = os.getenv('PRIMARY_BUCKET')
+
+print(bucket_name, "is the bucket")
 blobstore = S3Blobstore(bucket_name, access_key, secret_key)
 
 models_db = ModelsDatabase(blobstore)
@@ -31,8 +30,11 @@ def predict(model_id, version):
     except:
         # TODO: improve exception handling
         raise Exception('Invalid JSON body')
-    model = models_db.get_model(model_id, version)
-    features = fe.extract_features(model, req)
-    resp = model.predict(features)
+    model = models_db.get_model(model_id, version) # `model` is an instance of LudwigModel
+    features = fe.extract_features(model, req) # TODO: implement this
+    pred = model.predict(data_dict=[req]).to_dict('records')[0]
+    text = untokenize(pred)
+    resp = {"text":text}
+    print(resp)
     models_db.save_prediction(resp)
     return resp
