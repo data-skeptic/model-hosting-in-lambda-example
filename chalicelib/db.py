3d0b4003ee2e865307d1d7515b58c03d61272002
areeves87 <lxndr.rvs@gmail.com>
2020-04-16 15:11:07 -0700

    get and extract model files

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -1,5 +1,7 @@
 from chalicelib.model import Model
-
+import io
+import zipfile
+from ludwig.api import LudwigModel
 
 class ModelsDatabase(object):
 
@@ -10,19 +12,22 @@ class ModelsDatabase(object):
 
     # model = models_db.
     def get_model(self, model_id, version) -> Model:
-        key = f'{model_id}/{version}'
+        base = 'user/alex@dataskeptic.com/apps/forager' #TODO: discuss with kyle
+        key = f'{base}/{model_id}/{version}'
         # TODO: use MemCache?
         content = self.blobstore.get_blob(key)
-        # TODO: unpickle the content
-        # will be a zip file, so unzip and then get tf model
-        # tf models are not compatible with pickle
-        # use ludwig's methods to load tf models
-        # copy ludwig/ludwig folder into chalicelib to reduce deployment size
-        # will need tensorflow to load the model
-        # maybe can do with tensorflow lite to reduce deployment size
-        model = None
+        model_dir = self._extract_model_files(content)
+        model = LudwigModel.load(f'./{model_dir}')
         return model
 
     def save_prediction(self, prediction):
         pass
         # TODO: persist result
+
+    def _extract_model_files(self, content):
+        file_like_object = io.BytesIO(content)
+        zipfile_ob = zipfile.ZipFile(file_like_object)
+        model_dir = 'model/'
+        model_files = [x for x in zipfile_ob.namelist() if x.startswith(model_dir)]
+        zipfile_ob.extractall('./tmp', members=model_files)
+        return f'tmp/{model_dir}'
