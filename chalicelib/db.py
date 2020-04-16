62af06d3aacc047745159124ba24d98e5f9b8932
areeves87 <lxndr.rvs@gmail.com>
2020-04-16 13:08:31 -0700

    fix syntax

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -8,14 +8,21 @@ class ModelsDatabase(object):
         self.blobstore = blobstore
 
 
-    model = models_db.
+    # model = models_db.
     def get_model(self, model_id, version) -> Model:
         key = f'{model_id}/{version}'
         # TODO: use MemCache?
         content = self.blobstore.get_blob(key)
         # TODO: unpickle the content
-        model = ?
+        # will be a zip file, so unzip and then get tf model
+        # tf models are not compatible with pickle
+        # use ludwig's methods to load tf models
+        # copy ludwig/ludwig folder into chalicelib to reduce deployment size
+        # will need tensorflow to load the model
+        # maybe can do with tensorflow lite to reduce deployment size
+        model = None
         return model
 
     def save_prediction(self, prediction):
+        pass
         # TODO: persist result
