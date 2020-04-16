44aed6842064d271da30912eb33c35e1986bde42
areeves87 <lxndr.rvs@gmail.com>
2020-04-16 13:08:00 -0700

    add s3blobstore from feaas repo

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from chalice import Chalice
 from chalicelib.db import ModelsDatabase
 from chalicelib.features import FeatureExtractor
+from chalicelib.dao.blobstore.s3.s3blobstore import S3Blobstore
+
+import os
 
 app = Chalice(app_name='mhost')
 
-models_db = ModelsDatabase()
+if os.getenv('PRIMARY_BUCKET') is None:
+    from dotenv import load_dotenv
+    load_dotenv()
+
+access_key = os.getenv('AWS_ACCESS_KEY_ID')
+secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+bucket_name     = os.getenv('PRIMARY_BUCKET')
+blobstore = S3Blobstore(bucket_name, access_key, secret_key)
+
+models_db = ModelsDatabase(blobstore)
 fe = FeatureExtractor()
 
 @app.route('/')
