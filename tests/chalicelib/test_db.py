603185db9fa7bbb8e18d38dea04f3b6a2fd0999e
areeves87 <lxndr.rvs@gmail.com>
2020-09-26 21:28:41 -0700

    use monkeypatch to limit scope of patches to single tests

--- tests/chalicelib/test_db.py
+++ tests/chalicelib/test_db.py
@@ -17,16 +17,27 @@ def mockDocstore_init(self, table_name, access_key=None, secret_key=None):
     self.table = {}
     self.has_iam = False if access_key is None else True
 
+def mockModelsDB_init(self, blobstore, docstore):
+    self.blobstore = blobstore
+    self.docstore = docstore
+    self.cache = {}
+    self.status = {'status':'UNINITIALIZED'}
+
+def mockRun(self):
+	self.cache['test/latest'] = {}
+
 S3Blobstore.__init__ = mockS3_init
 Docstore.__init__ = mockDocstore_init
 
 blobstore = S3Blobstore('fake_bucket', 'foo', 'bar')
 docstore = Docstore('fake_table', 'foo', 'bar')
 
+ludwig_lookup = {"dest_key":"model.zip"}
+
 ludwig_record = """
 {
 "dest_key": "user/alex@dataskeptic.com/chitchat-dry-run.model.zip", 
-"model_id": "chitchat-dry-run.model/latest", 
+"model_id": "test/latest", 
 "object_id": "user/alex@dataskeptic.com/chitchat-dry-run.model.zip", 
 "key": "user/alex@dataskeptic.com/chitchat.csv", 
 "example1": "{\\"Question\\": \\"Can you ask me a question? \\"}", 
@@ -38,6 +49,8 @@ ludwig_record = """
 "type": "chalicelib.applications.ludwig.qa.Request"
 }
 """
+
+prophet_lookup = {"pickle_key":"data.prophet.pkl"}
 
 prophet_record = """
 {
@@ -75,14 +88,15 @@ def test_worker_extract_model_files():
 	shutil.rmtree(expected_dir)
 	assert model_object_id in model_dir
 
-def test_worker_load_model():
+def test_worker_load_model(monkeypatch):
 	from ludwig.api import LudwigModel
-	blobstore.get_blob = lambda x: b'fake content'
+	monkeypatch.setattr(blobstore,'get_blob', lambda x: b'fake content')
+	monkeypatch.setattr(WorkerThread, '_extract_model_files', lambda x, y, z: './models/mock/latest/model')
+	monkeypatch.setattr(LudwigModel, 'load', lambda x: {})
+	
 	worker = WorkerThread({'status':'UNINITIALIZED'}, {}, blobstore, docstore)
-	worker._extract_model_files = lambda x, y: './models/mock/latest/model'
 	record = jsons.loads(ludwig_record)
 	model_object_id = record['model_id']
-	LudwigModel.load = lambda x: {}
 	res = worker._load_model(record)
 	assert res
 	assert model_object_id in worker.cache
@@ -90,12 +104,13 @@ def test_worker_load_model():
 	res2 = worker._load_model(record) # exit early if model already loaded
 	assert worker.cache[model_object_id]['metadata']['marker'] == 'loaded'
 
-def test_worker_load_prophet_model():
+def test_worker_load_prophet_model(monkeypatch):
 	import pickle
-	pickle.loads = lambda x: {}
-	blobstore.get_blob = lambda x: b'fake content'
+	monkeypatch.setattr(pickle,'loads',lambda x: {})
+	monkeypatch.setattr(blobstore,'get_blob', lambda x: b'fake content')
+	monkeypatch.setattr(WorkerThread, '_extract_model_files', lambda x, y, z: './models/mock/latest/model')
+	
 	worker = WorkerThread({'status':'UNINITIALIZED'}, {}, blobstore, docstore)
-	worker._extract_model_files = lambda x, y: './models/mock/latest/model'
 	record = jsons.loads(prophet_record)
 	model_object_id = record['model_id']
 	res = worker._load_prophet_model(record)
@@ -105,15 +120,77 @@ def test_worker_load_prophet_model():
 	res2 = worker._load_prophet_model(record) # exit early if model already loaded
 	assert worker.cache[model_object_id]['metadata']['marker'] == 'loaded'
 
-def test_worker_run():
-	docstore.gsi_query = lambda x,y,z: []
+def test_worker_run(monkeypatch):
+	monkeypatch.setattr(docstore, 'gsi_query', lambda x,y,z: [])
+	monkeypatch.setattr(docstore, 'get_batch_documents', lambda keys: [])
+	
 	worker = WorkerThread({'status':'UNINITIALIZED'}, {}, blobstore, docstore)
 	worker.run()
 	assert 'INITIALIZED' in worker.status['status']
 
 
-def test_modelsdb_get_model():
+def test_modelsdb_get_model(monkeypatch):
+	from ludwig.api import LudwigModel
+	monkeypatch.setattr(blobstore,'get_blob', lambda x: b'fake content')
+	monkeypatch.setattr(docstore,'get_document', lambda x: docstore.table.get(x))
+	monkeypatch.setattr(WorkerThread, '_extract_model_files', lambda x, y, z: './models/mock/latest/model')
+	monkeypatch.setattr(LudwigModel, 'load', lambda x: {})
+	monkeypatch.setattr(ModelsDatabase,'__init__', mockModelsDB_init)
+	docstore.table['test/latest'] = ludwig_lookup
+	docstore.table['model.zip'] = jsons.loads(ludwig_record)
+	
 	modelsdb = ModelsDatabase(blobstore, docstore)
 	assert len(modelsdb.cache.keys()) == 0
-	# TODO: figure out how to make test_app/conftest.py not interfere with this ModelsDatabase class
-	# perhaps via more localized mocking?
+	modelsdb.get_model('test','latest')
+	assert 'test/latest' in modelsdb.cache
+
+
+def test_modelsdb_get_model_type(monkeypatch):
+	monkeypatch.setattr(ModelsDatabase,'__init__', mockModelsDB_init)
+	
+	modelsdb = ModelsDatabase(blobstore, docstore)
+	modelsdb.cache['test/latest'] = {"metadata":{"type": "chalicelib.applications.ludwig.qa.Request"}}
+	expected = "chalicelib.applications.ludwig.qa.Request"
+	assert modelsdb.get_model_type('test','latest') == expected
+
+
+def test_modelsdb_remove_model(monkeypatch):
+	monkeypatch.setattr(ModelsDatabase,'__init__', mockModelsDB_init)
+	
+	modelsdb = ModelsDatabase(blobstore, docstore)
+	modelsdb.cache['test/latest'] = {}
+	modelsdb.remove_model('test','latest')
+	assert 'test/latest' not in modelsdb.cache
+
+
+def test_modelsdb_load_model(monkeypatch):
+	from ludwig.api import LudwigModel
+	monkeypatch.setattr(blobstore,'get_blob', lambda x: b'fake content')
+	monkeypatch.setattr(docstore,'get_document', lambda x: docstore.table.get(x))
+	monkeypatch.setattr(WorkerThread, '_extract_model_files', lambda x, y, z: './models/mock/latest/model')
+	monkeypatch.setattr(LudwigModel, 'load', lambda x: {})
+	monkeypatch.setattr(ModelsDatabase,'__init__', mockModelsDB_init)
+	docstore.table['test/latest'] = ludwig_lookup
+	docstore.table['model.zip'] = jsons.loads(ludwig_record)
+	
+	modelsdb = ModelsDatabase(blobstore, docstore)
+	assert len(modelsdb.cache.keys()) == 0
+	modelsdb.load_model('test','latest')
+	assert 'test/latest' in modelsdb.cache
+
+
+def test_modelsdb_init(monkeypatch):
+	monkeypatch.setattr(WorkerThread, 'run', mockRun)
+	
+	modelsdb = ModelsDatabase(blobstore, docstore)
+	assert 'test/latest' in modelsdb.cache
+
+
+def test_modelsdb_reload_models(monkeypatch):
+	monkeypatch.setattr(WorkerThread, 'run', mockRun)
+	monkeypatch.setattr(ModelsDatabase,'__init__', mockModelsDB_init)
+	
+	modelsdb = ModelsDatabase(blobstore, docstore)
+	assert 'test/latest' not in modelsdb.cache
+	modelsdb.reload_models()
+	assert 'test/latest' in modelsdb.cache
